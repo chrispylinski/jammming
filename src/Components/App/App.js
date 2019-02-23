2201ac5977b7fb68dc3b73375f7cfc2c055dce6d
tacoder <chris.pylinski@googlemail.com>
2019-02-23 21:16:42 +0100

    inital component changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,20 +4,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          {/* Add a SearchBar component */}
+          <div class="App-playlist">
+            {/* Add a SearchResults component */}
+            {/* Add a Playlist component */}
+          </div>
+        </div>
       </div>
     );
   }
